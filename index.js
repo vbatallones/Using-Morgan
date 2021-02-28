b20bd2abc1d8838e328ff1ffae113872bde2d179
Levin <vbatallones.xvi@gmail.com>
2021-02-27 20:45:07 -0800

    utilizing the ap.use for every route to execute

--- index.js
+++ index.js
@@ -3,15 +3,44 @@ const app = express();
 const morgan = require('morgan');
 
 
-app.use(morgan('dev'))
+app.use(morgan('tiny'))
 
-app.get('/', (req,res) => {
+app.use((req, res, next) => {
+    console.log(req.method, req.path)
+    next();
+})
+
+// Middleware specific for the route /dogs
+app.use('/dogs', (req, res, next) => {
+    console.log('Hello I am a Dog!')
+    next();
+})
+
+// JUST A DEMO!
+
+// a function to verify a specific route if you add it in the route as a callback.
+// protecting a route.
+const verifyPassword = ((req, res, next) => {
+    const {password } = req.query;
+    password === 'mysecret' ? next() : res.send('Sorry you need a password');
+})
+
+app.get('/', (req, res) => {
     res.send('Homepage')
 })
-app.get('/dogs', (req,res) => {
+
+app.get('/dogs', (req, res) => {
     res.send('woof woof')
 })
 
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('My secret is: is a secret sorry LOL!')
+})
+
+// Error handling. If nothing has match
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND!')
+})
 app.listen(3001, () => {
     console.log('LISTENING TO PORT 3001')
 })
